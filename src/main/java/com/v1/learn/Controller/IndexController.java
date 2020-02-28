160324303b72cdea75a59d855f0215902ef9739f
mmmmute <546029685@qq.com>
2020-02-28 16:31:11 +0800

    bilibili P13 finished

--- src/main/java/com/v1/learn/Controller/IndexController.java
+++ src/main/java/com/v1/learn/Controller/IndexController.java
@@ -3,14 +3,12 @@ package com.v1.learn.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
